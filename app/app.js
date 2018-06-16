fd07fbf77fd883e2bf20e217cf794221a184dc09
Prashant Bhat <prashantbhat84@gmail.com>
2018-06-16 10:10:39 +0530

    error fixed in app.js file

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var greeter = require('./greeter.js')
-app.get('./',function(req,res){
+app.get('/',function(req,res){
     res.send(greeter());      
 });
 app.listen(process.env.PORT||3000,function(){
